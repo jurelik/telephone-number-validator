c73001b8eb82b142ff1afda8016d7b53c4e75669
jurelik <jure@likar.me>
2018-10-15 17:52:42 +0100

    JS passes the FCC challenge

--- tnv.js
+++ tnv.js
@@ -1,1 +1,32 @@
-console.log('sup');
+function telephoneCheck(str) {
+  let firstNum = /^[0-9]/;
+  let firstParanthesis = /^[(]/;
+  let firstArea = /^1\s|^1/;
+
+  let regexNum = /^([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
+  let regexParenthesis = /^([(][0-9][0-9][0-9][)])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
+
+  if (firstArea.test(str)) {
+    str = str.replace(firstArea, "");
+    if (regexNum.test(str) || regexParenthesis.test(str)) {
+      console.log(true);
+      return true;
+    }
+    console.log(false);
+    return false;
+  }
+  else if (firstNum.test(str)) {
+    console.log(regexNum.test(str));
+    return regexNum.test(str);
+  }
+  else if (firstParanthesis.test(str)) {
+    console.log(regexParenthesis.test(str));
+    return regexParenthesis.test(str);
+  }
+  else {
+    console.log(false);
+    return false;
+  }
+}
+
+telephoneCheck("555-555-5555");

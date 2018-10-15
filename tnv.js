253492109b0ffce9ceec6ff291f2759152120a4e
jurelik <jure@likar.me>
2018-10-15 18:26:14 +0100

    Optimised JS with fever lines of code

--- tnv.js
+++ tnv.js
@@ -1,32 +1,21 @@
 function telephoneCheck(str) {
-  let firstNum = /^[0-9]/;
-  let firstParanthesis = /^[(]/;
   let firstArea = /^1\s|^1/;
-
   let regexNum = /^([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
   let regexParenthesis = /^([(][0-9][0-9][0-9][)])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
 
   if (firstArea.test(str)) {
     str = str.replace(firstArea, "");
     if (regexNum.test(str) || regexParenthesis.test(str)) {
-      console.log(true);
       return true;
     }
-    console.log(false);
     return false;
   }
-  else if (firstNum.test(str)) {
-    console.log(regexNum.test(str));
-    return regexNum.test(str);
-  }
-  else if (firstParanthesis.test(str)) {
-    console.log(regexParenthesis.test(str));
-    return regexParenthesis.test(str);
+  else if (regexNum.test(str) || regexParenthesis.test(str)) {
+    return true;
   }
   else {
-    console.log(false);
     return false;
   }
 }
 
-telephoneCheck("555-555-5555");
+telephoneCheck("(555)555-5555");

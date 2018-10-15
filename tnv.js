569b32920adf828d88589dcd348fc151425ffb2e
jurelik <jure@likar.me>
2018-10-15 19:35:01 +0100

    Implemented asic UI and interaction with JS

--- tnv.js
+++ tnv.js
@@ -1,3 +1,5 @@
+// Telephone Number Validator
+
 function telephoneCheck(str) {
   let firstArea = /^1\s|^1/;
   let regexNum = /^([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
@@ -18,4 +20,25 @@ function telephoneCheck(str) {
   }
 }
 
-telephoneCheck("(555)555-5555");
+// DOM Interaction
+
+let form = document.getElementById('check-num');
+let input = document.getElementById('input-text');
+let result = document.getElementById('result');
+let btn = document.getElementById('submit-btn');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (input.value == "") {
+    result.innerHTML = `Please enter a telephone number`;
+  }
+  else if(telephoneCheck(input.value)) {
+    result.innerHTML = `${input.value} is a valid US telephone number`;
+  }
+  else {
+    result.innerHTML = `${input.value} is not a valid US telephone number`;
+  }
+
+  input.focus();
+});

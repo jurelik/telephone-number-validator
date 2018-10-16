dd4b37deaf8d9835cd86198ec8ccd0cf356e6202
jurelik <jure@likar.me>
2018-10-16 13:32:23 +0100

    Optimised JS by using just one regex

--- tnv.js
+++ tnv.js
@@ -1,23 +1,8 @@
 // Telephone Number Validator
 
 function telephoneCheck(str) {
-  let firstArea = /^1\s|^1/;
-  let regexNum = /^([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
-  let regexParenthesis = /^([(][0-9][0-9][0-9][)])(\s|-|)([0-9][0-9][0-9])(\s|-|)([0-9][0-9][0-9][0-9])$/;
-
-  if (firstArea.test(str)) {
-    str = str.replace(firstArea, "");
-    if (regexNum.test(str) || regexParenthesis.test(str)) {
-      return true;
-    }
-    return false;
-  }
-  else if (regexNum.test(str) || regexParenthesis.test(str)) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  let regexUlti = /^(1\s?)?(\(\d{3})\)|(\d{3})(\s|-)?(\d{3})(\s|-)?(\d{4})$/;
+  return regexUlti.test(str);
 }
 
 // DOM Interaction

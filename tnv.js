06be90ded6c7254cf164ffff4c9b2b9740ba75e2
jurelik <jure@likar.me>
2018-10-16 17:07:08 +0100

    Added phone wiggle and polished UI

--- tnv.js
+++ tnv.js
@@ -11,6 +11,9 @@ let form = document.getElementById('check-num');
 let input = document.getElementById('input-text');
 let result = document.getElementById('result');
 let btn = document.getElementById('submit-btn');
+let phone = document.getElementById('phone-icon');
+
+// On button click do the following
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -19,11 +22,19 @@ form.addEventListener('submit', e => {
     result.innerHTML = `Please enter a telephone number`;
   }
   else if(telephoneCheck(input.value)) {
-    result.innerHTML = `${input.value} is a valid US telephone number`;
+    result.innerHTML = `You entered a valid US telephone number`;
+    phone.classList.add('anim-enable'); // Animate a phone wiggle
   }
   else {
-    result.innerHTML = `${input.value} is not a valid US telephone number`;
+    result.innerHTML = `You entered a non-valid US telephone number`;
+    phone.classList.add('anim-enable'); // Animate a phone wiggle
   }
 
-  input.focus();
+  btn.blur();
+});
+
+// Reset phone wiggle
+
+phone.addEventListener('animationend', e => {
+  phone.classList.remove('anim-enable');
 });
